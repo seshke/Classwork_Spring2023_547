05f62eab327aed5ebe086a2c89e06ad58429867c
Aleksandra Koroza <ak559@duke.edu>
2023-01-27 12:15:33 -0500

    output fixed with string formatting

--- output_readability.py
+++ output_readability.py
@@ -2,5 +2,5 @@ weight = 20 / 2.205
 dosage = weight * 30
 
 print("CORRECT DOSAGE")
-print("For a patient weighing {} kg,".format(weight))
-print("  the correct dosage is {} mg the first day".format(dosage))
+print("For a patient weighing {:lf} kg,".format(weight))
+print("  the correct dosage is {:lf} mg the first day".format(dosage))
