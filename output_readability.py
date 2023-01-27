22f99379299fb1c792a728f370e4944e7a83e3ed
Aleksandra Koroza <ak559@duke.edu>
2023-01-27 12:17:13 -0500

    fix output format using round

--- output_readability.py
+++ output_readability.py
@@ -2,5 +2,5 @@ weight = 20 / 2.205
 dosage = weight * 30
 
 print("CORRECT DOSAGE")
-print("For a patient weighing {} kg,".format(weight))
+print("For a patient weighing {} kg,".format(round(weight)))
 print("  the correct dosage is {} mg the first day".format(dosage))
