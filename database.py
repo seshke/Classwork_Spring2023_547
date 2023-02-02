85e262b94edb9b326ccb2a2882595b84c6fe1881
Aleksandra Koroza <ak559@duke.edu>
2023-02-02 14:34:25 -0500

    able to get patient age given mrn

--- database.py
+++ database.py
@@ -8,6 +8,18 @@ def main_driver():
 	db.append(create_patient_entry("Save", 2, 45))
 	db.append(create_patient_entry("dave", 3, 50))
 	print(db)
+	print("Get patient Ann")
+	found_patient = get_patient_entry(4,db)
+	if found_patient is False: 
+		print("Patient mrn not found")
+	else:
+		print(found_patient)
+
+def get_patient_entry(mrn_to_find,db): 
+	for patient in db:
+		if patient[1] == mrn_to_find:
+			return patient
+	return False 
 	
 if __name__ == "__main__":
 	main_driver()
