7efce7a764a53f64f8da5c4a8fdf5436e76b1a62
Aleksandra Koroza <ak559@duke.edu>
2023-02-02 14:55:52 -0500

    patient rooms"

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 def create_patient_entry(patient_name, patient_mrn, patient_age):
-	new_patient = [patient_name, patient_mrn, patient_age] 
+	new_patient = [patient_name, patient_mrn, patient_age, []] 
 	return new_patient
 
 def main_driver():
@@ -8,18 +8,24 @@ def main_driver():
 	db.append(create_patient_entry("Save", 2, 45))
 	db.append(create_patient_entry("dave", 3, 50))
 	print(db)
-	print("Get patient Ann")
-	found_patient = get_patient_entry(4,db)
-	if found_patient is False: 
-		print("Patient mrn not found")
-	else:
-		print(found_patient)
+	add_test_to_patient(db, 1,"LDL",120)
+	room_numbers = ["103","232","333"]
+	print(db)
+	print_directory(db, room_numbers)
+
+def print_directory(db, room_numbers):
+	for i, patient in enumerate(db):
+		print("Patient {} is in room {}".format(patient[0], room_numbers[i]))
 
 def get_patient_entry(mrn_to_find,db): 
 	for patient in db:
 		if patient[1] == mrn_to_find:
 			return patient
 	return False 
+
+def add_test_to_patient(db, mrn, test_name, test_value):
+	patient = get_patient_entry(mrn, db)
+	patient[3].append([test_name, test_value])
 	
 if __name__ == "__main__":
 	main_driver()
