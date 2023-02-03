9351cada6444807d98826ee274913398d5351b7f
Aleksandra Koroza <ak559@duke.edu>
2023-02-03 13:05:18 -0500

    wrote unit tests to check new_func and pep8 styling

---
+++ test_new_func.py
@@ -0,0 +1,21 @@
+import pytest
+
+@pytest.mark.parametrize("HDL_input, expected",
+[(65,"Normal"),(45,"Borderline Low"),(20,"Low")])
+def test_HDL_analysis(HDL_input, expected):
+	from new_func import HDL_analysis
+	# Arrange
+	# Act 
+	answer = HDL_analysis(HDL_input)
+	# Assert
+	assert answer == expected 
+	
+@pytest.mark.parametrize("LDL_input, expected",
+[(200,"Very High"),(170,"High"),(140,"Borderline High"),(20,"Normal")])
+def test_LDL_analysis(LDL_input, expected):
+	from new_func import LDL_analysis
+	# Arrange
+	# Act 
+	answer = LDL_analysis(LDL_input)
+	# Assert
+	assert answer == expected 
