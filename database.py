63207e4bbf18175b75711df9aef31cecc651993b
Aleksandra Koroza <ak559@duke.edu>
2023-02-10 12:53:22 -0500

    fixed trailing whitespace and function spacing pep8

--- database.py
+++ database.py
@@ -14,38 +14,40 @@ def create_patient_entry_dict(first_name, last_name, MRN, Age):
 
 
 def main_driver():
-    db = []
-    db.append(create_patient_entry_dict("Ann",'Abbles', 1, 34))
-    db.append(create_patient_entry_dict("Save",'Help', 2, 45))
-    db.append(create_patient_entry_dict("dave",'davis', 3, 50))
+    db = {}
+    db[1] = create_patient_entry_dict("Ann", 'Abbles', 1, 34)
+    db[2] = create_patient_entry_dict("Save", 'Help', 2, 45)
+    db[3] = create_patient_entry_dict("dave", 'davis', 3, 50)
     print(db)
     print_database(db)
-    add_test_to_patient(db, 1,"LDL",120)
-    #room_numbers = ["103","232","333"]
-    #print(db)
-    #print_directory(db, room_numbers)
- 
-    
+    add_test_to_patient(db, 1, "LDL", 120)
+    # room_numbers = ["103","232","333"]
+    # print(db)
+    # print_directory(db, room_numbers)
+
+
 def get_full_name(patient_dict):
-    return patient_dict['First Name']+ ' ' + patient_dict['Last Name'] 
-   
- 
+    return patient_dict['First Name'] + ' ' + patient_dict['Last Name']
+
+
 def print_database(db):
-    for entry in db:
-        result = str(entry['MRN']) +", Full Name: " + get_full_name(entry)+ ", Age: " + str(entry['Age']) 
+    for entry in db.values():
+        print(entry)
+        result = str(entry['MRN']) + ", Full Name: " + get_full_name(entry)
+        + ", Age: " + str(entry['Age'])
         print(result)
-  
-        
+
+
 def print_directory(db, room_numbers):
     for i, patient in enumerate(db):
         print("Patient {} is in room {}".format(patient[0], room_numbers[i]))
 
 
-def get_patient_entry(mrn_to_find,db): 
-    for patient in db:
-        if patient['MRN'] == mrn_to_find:
-            return patient
-    return False 
+def get_patient_entry(mrn_to_find, db):
+    patient = db.get(mrn_to_find)
+    if patient is None:
+        return False
+    return patient
 
 
 def add_test_to_patient(db, mrn, test_name, test_value):
@@ -64,9 +66,7 @@ def get_test_result(db, mrn, test_name):
     patient = get_patient_entry(db, mrn)
     test_value = get_test_value_from_test_list(patient['Tests'], test_name)
     return test_value
-    
+
 
 if __name__ == "__main__":
     main_driver()
-    
-    
